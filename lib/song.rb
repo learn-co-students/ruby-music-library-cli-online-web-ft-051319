82a76e11428d94a74ae796a07ca66810f6591f9c
Travis Exline <t.exline22@gmail.com>
2019-05-30 19:19:14 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+  attr_accessor :name
+  attr_reader :genre, :artist
+
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if artist
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    song = self.new(name)
+    song.save
+    song
+  end
+end
