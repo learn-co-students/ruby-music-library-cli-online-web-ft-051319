518bbb0a1d11d0b59eb52e76e1ee06636f7a00aa
Haley DeKeyser <hcd09@live.com>
2019-06-02 05:06:25 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class MusicLibraryController
   attr_accessor :path
   
@@ -21,8 +23,62 @@ class MusicLibraryController
   end
   
   def list_songs
-    Song.all.sort_by(:name)
-    #each_with_index { |song, index| puts "#{index += 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}" }
+    sorted_songs = Song.all.uniq.sort { |song_a, song_b| song_a.name <=> song_b.name }
+    sorted_songs.each_with_index do |song, index|
+      song_artist = song.artist.name
+      song_genre = song.genre.name
+      song_name = song.name
+      puts "#{index + 1}. #{song_artist} - #{song_name} - #{song_genre}"
+    end
+  end
+  
+  def list_artists
+    sorted_artists = Artist.all.uniq.sort { |artist_a, artist_b| artist_a.name <=> artist_b.name }
+    sorted_artists.each_with_index do |artist, index|
+      puts "#{index + 1}. #{artist.name}"
+    end
+  end
+  
+  def list_genres
+    sorted_genres = Genre.all.uniq.sort { |genre_a, genre_b| genre_a.name <=> genre_b.name }
+    sorted_genres.each_with_index do |genre, index|
+      puts "#{index + 1}. #{genre.name}"
+    end
+  end
+  
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    user_input = gets.chomp
+    artist_requested = Artist.all.find { |artist| artist.name == user_input }
+    if artist_requested != nil
+      artist_requested.songs.sort! { |song_a, song_b| song_a.name <=> song_b.name }
+      artist_requested.songs.each_with_index do |song, index|
+      puts "#{index + 1}. #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+  
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    user_input = gets.chomp
+    genre_requested = Genre.all.find { |genre| genre.name == user_input }
+    if genre_requested != nil
+      genre_requested.songs.sort! { |song_a, song_b| song_a.name <=> song_b.name }
+      genre_requested.songs.each_with_index do |song, index|
+      puts "#{index + 1}. #{song.artist.name} - #{song.name}"
+      end
+    end
+  end
+  
+  def play_song
+    puts "Which song number would you like to play?"
+    user_input = gets.chomp.to_i
+    if (1..Song.all.uniq.size).include?(user_input) && user_input.is_a?(Integer)
+      sorted_songs = Song.all.uniq.sort { |song_a, song_b| song_a.name <=> song_b.name }
+      song_artist = sorted_songs[user_input - 1].artist.name
+      song_name = sorted_songs[user_input - 1].name
+      puts "Playing #{song_name} by #{song_artist}"
+    end
   end
   
 end
