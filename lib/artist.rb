fd3f739cc1cc394d96319bc13540420cd86585b6
Brian Salemi <briandsalemi@gmail.com>
2019-06-02 15:44:42 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+
+
+  def add_song(song)
+    if song.name ||= song.name = self
+      self.songs << song unless self.songs.include?(song)
+  end
+end
+end
---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song
+  attr_accessor :name
+  attr_reader :artist
+
+  @@all = []
+
+  def initialize(name, artist = nil)
+    @name = name
+    @artist = artist
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song
+  end
+
+  def artist=
+    @artist = Artist.new(name)
+    @artist.add_song(name)
+
+  end
+end
