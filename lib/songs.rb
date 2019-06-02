553686501acb6e3237ce5686efd2cce5daec6a85
Alethea Cary <aletheacary@gmail.com>
2019-06-02 20:11:08 +0000

    Done.

--- lib/songs.rb
+++ lib/songs.rb
@@ -1,14 +1,36 @@
 require_relative './parent.rb'
 
-class Song < Parent
-  attr_accessor :artist
-  
-  def initialize(name, artist = nil)
-    super(name)
-    @artist = artist
-    @artist.songs << self
-    
+class Song
+ attr_accessor :name
+ attr_reader :artist
+ 
+ @@all = []
+ 
+ def initialize(name, artist = nil)
+    @name = name
+    self.artist = artist
   end
   
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
   
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def self.create(name)
+    object_i = self.new(name)
+    object_i.save
+    object_i
+  end
 end
