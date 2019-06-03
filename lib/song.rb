84e5d011f70d2669525d2d1a179ab16a06023267
Brian Salemi <briandsalemi@gmail.com>
2019-06-03 15:21:55 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,9 @@ class Song
 
   def initialize(name, artist= nil, genre= nil)
     @name = name
-    self.artist = artist unless artist == nil
-    self.genre = genre unless artist == nil
+
+    self.artist = artist if artist != nil
+    self.genre = genre if genre != nil
   end
 
   def self.all
@@ -36,6 +37,10 @@ class Song
   def genre=(genre)
     @genre = genre
     genre.songs << self unless genre.songs.include?(self)
+  end
+
+  def self.find_by_name(name)
+    @@all.detect{|song| song.name == name}
   end
 
 
