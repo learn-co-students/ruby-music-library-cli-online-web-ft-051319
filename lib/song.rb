41698836081b11b5c2cec6d3a07af6779990738b
Brian Salemi <briandsalemi@gmail.com>
2019-06-03 15:23:36 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -43,5 +43,13 @@ class Song
     @@all.detect{|song| song.name == name}
   end
 
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
 
 end
