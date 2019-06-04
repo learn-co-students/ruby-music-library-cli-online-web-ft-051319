424db3f4e64b32f62fa2e42d43719e22e773225d
Brian Salemi <briandsalemi@gmail.com>
2019-06-04 02:28:31 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -47,10 +47,33 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     input = gets.strip
     if artist = Artist.find_by_name(input)
-    artist.songs.sort_by(&:name).each_with_index(1) do |song, index|
-      puts "#{index}. #{song.name} - #{song.genre.name}"
+    artist.songs.sort_by(&:name).each_with_index do |song, index|
+      puts "#{index + 1}. #{song.name} - #{song.genre.name}"
     end
   end
+end
+
+def list_songs_by_genre
+  puts "Please enter the name of a genre:"
+  input = gets.strip
+  if genre = Genre.find_by_name(input)
+    genre.songs.sort_by(&:name).each_with_index do |song, index|
+      puts "#{index + 1}. #{song.artist.name} - #{song.name}"
+    end
+  end
+end
+
+def play_song
+
+  listed_songs = Song.all.sort{|a, b| a.name <=> b.name}
+
+  puts "Which song number would you like to play?"
+  input = gets.strip.to_i
+    if (1..Song.all.length).include?(input)
+    song = listed_songs[input - 1]
+  puts "Playing #{song.name} by #{song.artist.name}"
+
+end
 end
 
 
